797e17ebc0e50598dbac3b26522f5e9415162185
ronoctua <git.noctua@gmail.com>
2024-06-14 22:08:53 +0100

    fix: makes inline SVG icons visible in Safari browser

--- src/media/svg/outline/whatsapp.svg.tsx
+++ src/media/svg/outline/whatsapp.svg.tsx
@@ -8,6 +8,8 @@
  * ```
  */
 export const WhatsAppSVG = ({
+  width = '25',
+  height = '25',
   viewBox = '0 0 24 24',
   fill = 'currentColor',
   strokeWidth = '0',
@@ -17,6 +19,8 @@ export const WhatsAppSVG = ({
 }: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
     viewBox={viewBox}
     strokeLinecap={strokeLinecap}
     strokeLinejoin={strokeLinejoin}
